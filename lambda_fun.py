669de13739f491b09240676315e0c82af7b2477d
Rajkumar Singh <singh.rajkumar@siemens.com>
2023-04-20 23:21:20 +0530

    added lines in file

--- lambda_fun.py
+++ lambda_fun.py
@@ -1,21 +1,23 @@
 import requests
 import json
+import http.client
 
 def lambda(event, context):
 
-  payload = {
-    "subnet_id": "aws_subnet.private.id",
-    "name": "Rajkumar Singh",
-    "email": "rajkumarsingh07@gmail.com"
-  }
+    payload = {
+      "subnet_id": "aws_subnet.private.id",
+      "name": "Rajkumar Singh",
+      "email": "rajkumarsingh07@gmail.com"
+    }
 
-  headers = {
-    'X-Siemens-Auth': 'test'
-  }
+    headers = {
+      'X-Siemens-Auth': 'test'
+    }
 
-  url = '/candidate-email_serverless_lambda_stage/data'
+    url = '/candidate-email_serverless_lambda_stage/data'
 
-response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
 
-print(response.status_code)
-print(response.json())
+    print(response.status_code)
+    print(response.json())
+return result
